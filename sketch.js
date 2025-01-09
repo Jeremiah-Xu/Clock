688c37c66cdda90103b063824fe21c233b4696fd
Jeremiah-Xu <jx2515@columbia.edu>
2025-01-09 19:29:44 +0800

    Update2.0 sketch.js

--- sketch.js
+++ sketch.js
@@ -20,38 +20,51 @@ function draw() {
   
   rotate(-90);
   
-  let h = hour();
-  let m = minute();
-  let s = second();
-  
-  fill(255);
-  textFont(customFont); 
-  strokeWeight(1);
-  stroke(255);
-  rotate(90);
-  textSize(25);
-  output = str(h)+" : "+  str(m)  +" : "+  str(s);
-  text(output,0,0);
-  rotate(-90);
+  let h = doubleDigits(hour());
+  let m = doubleDigits(minute());
+  let s = doubleDigits(second());
   
   
   //anolgue clock
   
-  strokeWeight(10);
+  strokeWeight(30);
   noFill();
   
   let msg = str(h)+" : "+  str(m)  +" : "+  str(s);
    
   
-  stroke(150,250,255);
+  stroke(50,240,255);
   let secondAngle = map(s, 0, 60, 0, 360);
-  arc(0, 0, 360, 360, 0, secondAngle);
+  arc(0, 0, 375, 375, 0, secondAngle);
   
-  stroke(150,150,255);
+  stroke(100,170,255);
   let minuteAngle = map(m+s/60, 0, 60, 0, 360);
-  arc(0, 0, 330, 330, 0, minuteAngle);
+  arc(0, 0, 280, 280, 0, minuteAngle);
   
   stroke(200,100,255);
-  let hourAngle = map(h+m/60+s/3600, 0, 12, 0, 360);
-  arc(0, 0, 300, 300, 0, hourAngle);
+  let hourAngle = map(h%12+m/60+s/3600, 0, 12, 0, 360);
+  arc(0, 0, 185, 185, 0, hourAngle);
+  
+  //digital display
+  fill(255);
+  textFont(customFont); 
+  strokeWeight(2);
+  stroke(255);
+  textSize(25);
+  output = str(h)+" : "+  str(m)  +" : "+  str(s);
+  rotate(s*6);
+  text(output,80,36);
+  
+  
+}
+
+function doubleDigits(integer) {
+  let intToString = integer.toString();
+  let msg; 
+  if (intToString.length == 1) {
+    msg = "0"+intToString;
+  } else {
+    msg = intToString;
+  }
+  return msg;
 }
